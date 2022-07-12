50198a7a509b22b7319ad91c332d4ab1258d9bbc
ANUSHA PATIL <104024116+ANUSHA2208@users.noreply.github.com>
2022-07-12 11:53:33 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
       
 $(".button-1").click(function()
 {
-    window.location.href = "http://127.0.0.1:5500/index.html";
+    window.location.href = "https://anusha2208.github.io/Age-Calulator/";
 });
 
 
